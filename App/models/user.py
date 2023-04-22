d0fb65a621e5ddbe9114469bc766c13bc11529de
P-jay76 <prestonajoseph@gmail.com>
2023-04-22 02:40:34 +0000

    add reg user

--- App/models/user.py
+++ App/models/user.py
@@ -4,7 +4,8 @@ from App.database import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False, unique=True)
+    username =  db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(90), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
 
     def __init__(self, username, password):
@@ -24,4 +25,17 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
+class RegularUser(User):
+  __tablename__='regular_user'
+  competitions = db.relationship('Competition', backref='user', lazy=True) # sets up a relationship to competitions which references User
+    
+    def get_json(self):
+    return {
+      "id": self.id,
+      "username": self.username,
+      "email": self.email,
+      "role": 'regular user'
+    }
 
+  def __repr__(self):
+    return f'<RegularUser {self.id} : {self.username} - {self.email}>'
